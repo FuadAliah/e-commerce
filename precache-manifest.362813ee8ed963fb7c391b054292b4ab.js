f040814710b8c6cc52f9e0781918d048244b355a
Fuad Aliah <eye.design17@yahoo.com>
2021-05-04 01:03:27 +0300

    Updates

---
+++ precache-manifest.362813ee8ed963fb7c391b054292b4ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4517ecf36b77402501119f0719489736",
+    "url": "/e-commerce/index.html"
+  },
+  {
+    "revision": "fdd6d0ac8486a5179d9a",
+    "url": "/e-commerce/static/css/main.b993d028.chunk.css"
+  },
+  {
+    "revision": "10904eeff39522c78463",
+    "url": "/e-commerce/static/js/2.612eb8f3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/e-commerce/static/js/2.612eb8f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fdd6d0ac8486a5179d9a",
+    "url": "/e-commerce/static/js/main.9843da8c.chunk.js"
+  },
+  {
+    "revision": "f0bd369c1ff9feedfaf5",
+    "url": "/e-commerce/static/js/runtime-main.96a20351.js"
+  }
+]);
